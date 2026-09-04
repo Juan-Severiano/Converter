47b6aa69b39e1f7801fe501465eca015c0f84452
Francisco Juan <juansoussev@gmail.com>
2026-09-03 22:19:03 -0300

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Francisco Juan on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
